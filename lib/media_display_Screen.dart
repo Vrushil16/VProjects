7190319e1aa14bc15049b8120a05f761a0a111a9
Vrushil <vrushilparekh@hjsysweb.com>
2024-08-28 10:38:51 +0530

    Second

--- lib/media_display_Screen.dart
+++ lib/media_display_Screen.dart
@@ -1,10 +1,40 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:v_projects/utils/responsive_widget.dart';
-import 'package:video_player/video_player.dart';
+
+class ProjectListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Projects')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('CCC'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MediaDisplayScreen(
+                    folderPath: 'Projects/P1/P_I_V_P',
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class MediaDisplayScreen extends StatefulWidget {
+  final String folderPath;
+
+  MediaDisplayScreen({required this.folderPath});
+
   @override
   _MediaDisplayScreenState createState() => _MediaDisplayScreenState();
 }
@@ -22,7 +52,7 @@ class _MediaDisplayScreenState extends State<MediaDisplayScreen> {
   }
 
   Future<void> fetchFiles() async {
-    final ListResult result = await FirebaseStorage.instance.ref('Projects/P1/P_I_V_P').listAll();
+    final ListResult result = await FirebaseStorage.instance.ref(widget.folderPath).listAll();
 
     List<String> images = [];
     List<String> videos = [];
@@ -50,100 +80,98 @@ class _MediaDisplayScreenState extends State<MediaDisplayScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Media Display")),
+      appBar: AppBar(
+        title: Text("Media Display", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueGrey,
+      ),
       body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ResponsiveWidget(
-              largeScreen: buildLargeScreen(),
-              mediumScreen: buildMediumScreen(),
-              smallScreen: buildSmallScreen(),
+          ? Center(child: CircularProgressIndicator(color: Colors.blueGrey))
+          : LayoutBuilder(
+              builder: (context, constraints) {
+                if (constraints.maxWidth >= 1200) {
+                  return buildGridView(crossAxisCount: 4);
+                } else if (constraints.maxWidth >= 800) {
+                  return buildGridView(crossAxisCount: 3);
+                } else {
+                  return buildListView();
+                }
+              },
             ),
     );
   }
 
-  Widget buildLargeScreen() {
+  Widget buildGridView({required int crossAxisCount}) {
     return GridView.builder(
+      padding: EdgeInsets.all(10),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4,
+        crossAxisCount: crossAxisCount,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
       itemCount: imageUrls.length + videoUrls.length + pdfUrls.length,
       itemBuilder: (context, index) {
         if (index < imageUrls.length) {
-          return Image.network(imageUrls[index], fit: BoxFit.cover);
+          return buildMediaCard(url: imageUrls[index], mediaType: 'image');
         } else if (index < imageUrls.length + videoUrls.length) {
-          return buildVideoPlayer(videoUrls[index - imageUrls.length]);
+          return buildMediaCard(url: videoUrls[index - imageUrls.length], mediaType: 'video');
         } else {
-          return buildPdfViewer(pdfUrls[index - imageUrls.length - videoUrls.length]);
+          return buildMediaCard(url: pdfUrls[index - imageUrls.length - videoUrls.length], mediaType: 'pdf');
         }
       },
     );
   }
 
-  Widget buildMediumScreen() {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-      itemCount: imageUrls.length + videoUrls.length + pdfUrls.length,
-      itemBuilder: (context, index) {
-        if (index < imageUrls.length) {
-          return Image.network(imageUrls[index], fit: BoxFit.cover);
-        } else if (index < imageUrls.length + videoUrls.length) {
-          return buildVideoPlayer(videoUrls[index - imageUrls.length]);
-        } else {
-          return buildPdfViewer(pdfUrls[index - imageUrls.length - videoUrls.length]);
-        }
-      },
-    );
-  }
-
-  Widget buildSmallScreen() {
+  Widget buildListView() {
     return ListView.builder(
+      padding: EdgeInsets.all(10),
       itemCount: imageUrls.length + videoUrls.length + pdfUrls.length,
       itemBuilder: (context, index) {
         if (index < imageUrls.length) {
-          return Image.network(imageUrls[index], fit: BoxFit.cover);
+          return buildMediaCard(url: imageUrls[index], mediaType: 'image', isListView: true);
         } else if (index < imageUrls.length + videoUrls.length) {
-          return buildVideoPlayer(videoUrls[index - imageUrls.length]);
+          return buildMediaCard(url: videoUrls[index - imageUrls.length], mediaType: 'video', isListView: true);
         } else {
-          return buildPdfViewer(pdfUrls[index - imageUrls.length - videoUrls.length]);
+          return buildMediaCard(url: pdfUrls[index - imageUrls.length - videoUrls.length], mediaType: 'pdf', isListView: true);
         }
       },
     );
   }
 
-  Widget buildVideoPlayer(String url) {
-    VideoPlayerController controller = VideoPlayerController.network(url);
-    return FutureBuilder(
-      future: controller.initialize(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: VideoPlayer(controller),
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
+  Widget buildMediaCard({required String url, required String mediaType, bool isListView = false}) {
+    return GestureDetector(
+      onTap: () => _launchUrl(url),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 5,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              CachedNetworkImage(
+                imageUrl: url,
+                fit: BoxFit.cover,
+                height: isListView ? 150 : double.infinity,
+                width: double.infinity,
+                placeholder: (context, url) => Center(child: CircularProgressIndicator(color: Colors.blueGrey)),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+              if (mediaType == 'video') Icon(Icons.play_circle_outline, color: Colors.white, size: 50),
+              if (mediaType == 'pdf') Icon(Icons.picture_as_pdf, color: Colors.redAccent, size: 50),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
-  Widget buildPdfViewer(String url) {
-    return ElevatedButton(
-      onPressed: () async {
-        final Uri pdfUri = Uri.parse(url);
-        if (await canLaunch(pdfUri.toString())) {
-          await launch(pdfUri.toString());
-        } else {
-          throw 'Could not launch $pdfUri';
-        }
-      },
-      child: Text("View PDF"),
-    );
+  void _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
